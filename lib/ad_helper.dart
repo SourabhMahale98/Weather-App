6a19c1ab2baf8a09990930409cfd6423198bb757
Sourabh Mahale <SourabhMahale98@gmail.com>
2021-07-10 16:05:23 +0530

    Update ad_helper.dart

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -3,17 +3,11 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-5562364405868344/6631903449";
+      return "";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
   }
 
-  // static String get nativeAdUnitId {
-  //   if (Platform.isAndroid) {
-  //     return "ca-app-pub-3940256099942544/2247696110";
-  //   } else {
-  //     throw new UnsupportedError("Unsupported platform");
-  //   }
-  // }
-}
+
+  
